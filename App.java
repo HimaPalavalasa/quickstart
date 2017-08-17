3c0628b0c2727b581197641dfd177f499793beb7
Hima Palavalasa <31088405+HimaPalavalasa@users.noreply.github.com>
2017-08-17 10:40:41 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,14 @@
+package com.vogella.maven.quickstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//Gson gson = new Gson();
+        System.out.println("Hello World!");
+    }
+}
